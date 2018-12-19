7802ef948645e8e93eb75acb5c2d77a34e9ccf31
Michael Krog <michaelryankrog@gmail.com>
2018-12-19 08:58:53 -0700

    updates

--- idea.js
+++ idea.js
@@ -3,17 +3,10 @@ class Idea {
     this.key = key;
     this.title = title;
     this.body = body;
-    console.log(title, body);
-    // console.log("constructor call");
-    // console.log(cardArray);
   }
 
   saveToStorage() {
-    console.log("in save storage " + this);
-    // var stringifiedObj = JSON.stringify(this);
     localStorage.setItem(this.key, JSON.stringify(this));
-    // console.log("Save to storage called");
-    // cardArray.push(this);
   }
 
   deleteFromStorage() {
